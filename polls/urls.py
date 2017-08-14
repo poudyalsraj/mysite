519ded4e9ddeecdbcb4f4e50fc04b46118b594e8
poudyalsraj <poudyalsraj@gmail.com>
2017-08-14 11:46:14 +0545

    works on views

---
+++ polls/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+
+    url(r'^$', views.CategoryListView.as_view(), name='home'),
+    url(r'^(?P<pk>\d+)/$', views.QuestionsView.as_view(), name='ques'),
+    url(r'^(?P<c_id>\d+)/(?P<pk>\d+)$', views.QuestionsDetailView.as_view(), name='ques-choice'),
+    # url(r'^\d/(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultView.as_view(), name='result'),
+
+
+    # url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,136 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+# from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Category, Question, Choice
+from django.urls import reverse
+
+
+class CategoryListView(ListView):
+
+    template_name = 'polls/home.html'
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        return context
+
+
+class QuestionsView(DetailView):
+
+    model = Category
+    template_name = 'polls/ques.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(QuestionsView, self).get_context_data(**kwargs)
+        context['ques'] = Question.objects.all()
+        return context
+
+
+class QuestionsDetailView(DetailView):
+    model = Question
+    template_name = 'polls/ques_choice.html'
+
+    def get_context_data(self, **kwargs):
+
+        context = super(QuestionsDetailView, self).get_context_data(**kwargs)
+        context['choices'] = Choice.objects.all()
+        return context
+
+
+class ResultView(TemplateView):
+    template_name = 'polls/results.html'
+    model = Choice
+
+    def post(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice.votes += 1
+        selected_choice.save()
+        context = {
+
+            'choices': Choice.objects.all(),
+
+        }
+        return super(TemplateView, self).render_to_response(context)
+
+
+    # def get_context_data(self, **kwargs):
+
+    #     context = super(ResultView, self).get_context_data(**kwargs)
+    #     context['choices'] = Choice.objects.all()
+    #     return context
+
+
+
+
+
+
+
+
+
+
+# def vote(request, question_id):
+#     print("**************************************88")
+#     question = get_object_or_404(Question, pk=question_id)
+#     print("hellow")
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/ques_choice.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
